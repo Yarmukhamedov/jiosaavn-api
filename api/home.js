22ce5a00face49ca21f3eeaaae9bea01b3ea4c9e
OD SKYLER <akashpati89@gmail.com>
2026-06-13 19:54:24 +0530

    Update home.js

--- api/home.js
+++ api/home.js
@@ -266,6 +266,23 @@ export default async function handler(
   req,
   res
 ) {
+
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "*"
+  );
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  
   const lang =
     req.query.lang || "english";
 

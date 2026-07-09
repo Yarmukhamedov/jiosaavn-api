929a96ce09f89f76797fa576524af2972d56efd6
OD SKYLER <akashpati89@gmail.com>
2026-07-09 13:09:20 +0530

    Update artist.js

--- api/artist.js
+++ api/artist.js
@@ -267,7 +267,7 @@ export default async function handler(
     `&category=` +
     `&sort_order=` +
     `&includeMetaTags=0` +
-    `&ctx=wap6dot0` +
+    `&ctx=web6dot0` +
     `&api_version=4` +
     `&_format=json` +
     `&_marker=0`;
@@ -284,7 +284,7 @@ export default async function handler(
     `&category=latest` +
     `&sort_order=desc` +
     `&includeMetaTags=0` +
-    `&ctx=wap6dot0` +
+    `&ctx=web6dot0` +
     `&api_version=4` +
     `&_format=json` +
     `&_marker=0`;
